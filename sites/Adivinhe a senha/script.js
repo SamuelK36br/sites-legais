40a4596d1b4c0d36b7b0a83135bcecd55428fae0
SamuelK36br <samuelbernardo091@gmail.com>
2025-01-05 17:41:29 -0300

    Add files via upload

---
+++ sites/Adivinhe a senha/script.js
@@ -0,0 +1,64 @@
+function enigmaUm(){
+    const inputEnigmaUm = document.getElementById("inputEnigmaUm").value;
+    if (inputEnigmaUm === "472"){
+        document.getElementById("respostaUm").innerHTML="A senha está CORRETA!</em>";
+    } else {
+        document.getElementById("respostaUm").innerHTML="A senha está ERRADA!";
+    }
+}
+function enigmaDois(){
+    const inputEnigmaDois = document.getElementById("inputEnigmaDois").value;
+    if (inputEnigmaDois === "095"){
+        document.getElementById("respostaDois").innerHTML="A senha está CORRETA!";
+    } else {
+        document.getElementById("respostaDois").innerHTML="A senha está ERRADA!";
+    }
+}
+function enigmaTres(){
+    const inputEnigmaTres = document.getElementById("inputEnigmaTres").value;
+    if (inputEnigmaTres === "713"){
+        document.getElementById("respostaTres").innerHTML="A senha está CORRETA!";
+    } else {
+        document.getElementById("respostaTres").innerHTML="A senha está ERRADA!";
+    }
+}
+function enigmaQuatro(){
+    const inputEnigmaQuatro = document.getElementById("inputEnigmaQuatro").value;
+    if (inputEnigmaQuatro === "473"){
+        document.getElementById("respostaQuatro").innerHTML="A senha está CORRETA!";
+    } else {
+        document.getElementById("respostaQuatro").innerHTML="A senha está ERRADA!";
+    }
+}
+function enigmaCinco(){
+    const inputEnigmaCinco = document.getElementById("inputEnigmaCinco").value;
+    if (inputEnigmaCinco === "459"){
+        document.getElementById("respostaCinco").innerHTML="A senha está CORRETA!";
+    } else {
+        document.getElementById("respostaCinco").innerHTML="A senha está ERRADA!";
+    }
+}
+function enigmaSeis(){
+    const inputEnigmaSeis = document.getElementById("inputEnigmaSeis").value;
+    if (inputEnigmaSeis === "042"){
+        document.getElementById("respostaSeis").innerHTML="A senha está CORRETA!";
+    } else {
+        document.getElementById("respostaSeis").innerHTML="A senha está ERRADA!";
+    }
+}
+function enigmaSete(){
+    const inputEnigmaSete = document.getElementById("inputEnigmaSete").value;
+    if (inputEnigmaSete === "139"){
+        document.getElementById("respostaSete").innerHTML="A senha está CORRETA!";
+    } else {
+        document.getElementById("respostaSete").innerHTML="A senha está ERRADA!";
+    }
+}
+function enigmaBonus(){
+    const inputEnigmaBonus = document.getElementById("inputEnigmaBonus").value;
+    if (inputEnigmaBonus === "0953"){
+        document.getElementById("respostaBonus").innerHTML="A senha está CORRETA!";
+    } else {
+        document.getElementById("respostaBonus").innerHTML="A senha está ERRADA!";
+    }
+}
