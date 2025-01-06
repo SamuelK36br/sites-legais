1fe9f43017174a9a4fc48ee7f4f822e594fb5a22
SamuelK36br <samuelbernardo091@gmail.com>
2025-01-06 20:37:40 -0300

    Add files via upload

--- sites/Adivinhe a senha/script.js
+++ sites/Adivinhe a senha/script.js
@@ -1,72 +1,75 @@
+const fotoCadeado = document.getElementsByClassName("cadeado");
+const resposta = document.getElementsByClassName("resposta");
+
 function enigmaUm(){
     const inputEnigmaUm = document.getElementById("inputEnigmaUm").value;
     if (inputEnigmaUm === "472"){
-        document.getElementById("respostaUm").innerHTML="A senha está CORRETA!</em>";
-        document.getElementById("cadeadoUm").src="https://i.imgur.com/LusLjmS.png";
+        resposta[0].innerHTML="A senha está CORRETA!";
+        fotoCadeado[0].src="https://i.imgur.com/LusLjmS.png";
     } else {
-        document.getElementById("respostaUm").innerHTML="A senha está ERRADA!";
+        resposta[0].innerHTML="A senha está ERRADA!";
     }
 }
 function enigmaDois(){
     const inputEnigmaDois = document.getElementById("inputEnigmaDois").value;
     if (inputEnigmaDois === "095"){
-        document.getElementById("respostaDois").innerHTML="A senha está CORRETA!";
-        document.getElementById("cadeadoDois").src="https://i.imgur.com/LusLjmS.png";
+        resposta[1].innerHTML="A senha está CORRETA!";
+        fotoCadeado[1].src="https://i.imgur.com/LusLjmS.png";
     } else {
-        document.getElementById("respostaDois").innerHTML="A senha está ERRADA!";
+        resposta[1].innerHTML="A senha está ERRADA!";
     }
 }
 function enigmaTres(){
     const inputEnigmaTres = document.getElementById("inputEnigmaTres").value;
     if (inputEnigmaTres === "713"){
-        document.getElementById("respostaTres").innerHTML="A senha está CORRETA!";
-        document.getElementById("cadeadoTres").src="https://i.imgur.com/LusLjmS.png"
+        resposta[2].innerHTML="A senha está CORRETA!";
+        fotoCadeado[2].src="https://i.imgur.com/LusLjmS.png";
     } else {
-        document.getElementById("respostaTres").innerHTML="A senha está ERRADA!";
+        resposta[2].innerHTML="A senha está ERRADA!";
     }
 }
 function enigmaQuatro(){
     const inputEnigmaQuatro = document.getElementById("inputEnigmaQuatro").value;
     if (inputEnigmaQuatro === "473"){
-        document.getElementById("respostaQuatro").innerHTML="A senha está CORRETA!";
-        document.getElementById("cadeadoQuatro").src="https://i.imgur.com/LusLjmS.png"
+        resposta[3].innerHTML="A senha está CORRETA!";
+        fotoCadeado[3].src="https://i.imgur.com/LusLjmS.png";
     } else {
-        document.getElementById("respostaQuatro").innerHTML="A senha está ERRADA!";
+        resposta[3].innerHTML="A senha está ERRADA!";
     }
 }
 function enigmaCinco(){
     const inputEnigmaCinco = document.getElementById("inputEnigmaCinco").value;
     if (inputEnigmaCinco === "459"){
-        document.getElementById("respostaCinco").innerHTML="A senha está CORRETA!";
-        document.getElementById("cadeadoCinco").src="https://i.imgur.com/LusLjmS.png"
+        resposta[4].innerHTML="A senha está CORRETA!";
+        fotoCadeado[4].src="https://i.imgur.com/LusLjmS.png";
     } else {
-        document.getElementById("respostaCinco").innerHTML="A senha está ERRADA!";
+        resposta[4].innerHTML="A senha está ERRADA!";
     }
 }
 function enigmaSeis(){
     const inputEnigmaSeis = document.getElementById("inputEnigmaSeis").value;
     if (inputEnigmaSeis === "042"){
-        document.getElementById("respostaSeis").innerHTML="A senha está CORRETA!";
-        document.getElementById("cadeadoSeis").src="https://i.imgur.com/LusLjmS.png"
+        resposta[5].innerHTML="A senha está CORRETA!";
+        fotoCadeado[5].src="https://i.imgur.com/LusLjmS.png";
     } else {
-        document.getElementById("respostaSeis").innerHTML="A senha está ERRADA!";
+        resposta[5].innerHTML="A senha está ERRADA!";
     }
 }
 function enigmaSete(){
     const inputEnigmaSete = document.getElementById("inputEnigmaSete").value;
     if (inputEnigmaSete === "139"){
-        document.getElementById("respostaSete").innerHTML="A senha está CORRETA!";
-        document.getElementById("cadeadoSete").src="https://i.imgur.com/LusLjmS.png"
+        resposta[6].innerHTML="A senha está CORRETA!";
+        fotoCadeado[6].src="https://i.imgur.com/LusLjmS.png";
     } else {
-        document.getElementById("respostaSete").innerHTML="A senha está ERRADA!";
+        resposta[6].innerHTML="A senha está ERRADA!";
     }
 }
 function enigmaBonus(){
     const inputEnigmaBonus = document.getElementById("inputEnigmaBonus").value;
     if (inputEnigmaBonus === "0953"){
-        document.getElementById("respostaBonus").innerHTML="A senha está CORRETA!";
-        document.getElementById("cadeadoBonus").src="https://i.imgur.com/LusLjmS.png"
+        resposta[7].innerHTML="A senha está CORRETA!";
+        fotoCadeado[7].src="https://i.imgur.com/LusLjmS.png";
     } else {
-        document.getElementById("respostaBonus").innerHTML="A senha está ERRADA!";
+        resposta[7].innerHTML="A senha está ERRADA!";
     }
 }
