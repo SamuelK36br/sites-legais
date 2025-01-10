a082b2c9d8e39052278e3acaaba567384229471b
SamuelK36br <samuelbernardo091@gmail.com>
2025-01-10 18:52:10 -0300

    HTML, CSS E JS de "Adivinhe a Senha 2"

---
+++ sites/Adivinhe a Senha 2/script.js
@@ -0,0 +1,95 @@
+const input = document.getElementsByTagName("input");
+const resposta = document.getElementsByClassName("resposta");
+const imagemPasta = document.getElementsByClassName("imgPasta");
+const certoOuErrado = document.getElementsByClassName("certoOuErrado");
+
+function enigmaUm(){
+    if (input[0].value === "321"){
+        resposta[0].innerHTML = "A senha está CORRETA!";
+        imagemPasta[0].src="https://i.imgur.com/6yfWk07.png";
+        certoOuErrado[0].src="https://win98icons.alexmeub.com/icons/png/trust0-0.png";
+    } else {
+        resposta[0].innerHTML = "A senha está ERRADA!";
+        certoOuErrado[0].src="https://win98icons.alexmeub.com/icons/png/msg_error-0.png";
+    }
+}
+function enigmaDois(){
+    if (input[1].value === "725"){
+        resposta[1].innerHTML = "A senha está CORRETA!";
+        imagemPasta[1].src="https://i.imgur.com/6yfWk07.png";
+        certoOuErrado[1].src="https://win98icons.alexmeub.com/icons/png/trust0-0.png";
+    } else {
+        resposta[1].innerHTML = "A senha está ERRADA!";
+        certoOuErrado[1].src="https://win98icons.alexmeub.com/icons/png/msg_error-0.png";
+    }
+}
+function enigmaTres(){
+    if (input[2].value === "601"){
+        resposta[2].innerHTML = "A senha está CORRETA!";
+        imagemPasta[2].src="https://i.imgur.com/6yfWk07.png";
+        certoOuErrado[2].src="https://win98icons.alexmeub.com/icons/png/trust0-0.png";
+    } else {
+        resposta[2].innerHTML = "A senha está ERRADA!";
+        certoOuErrado[2].src="https://win98icons.alexmeub.com/icons/png/msg_error-0.png";
+    }
+}
+function enigmaQuatro(){
+    if (input[3].value === "483"){
+        resposta[3].innerHTML = "A senha está CORRETA!";
+        imagemPasta[3].src="https://i.imgur.com/6yfWk07.png";
+        certoOuErrado[3].src="https://win98icons.alexmeub.com/icons/png/trust0-0.png";
+    } else {
+        resposta[3].innerHTML = "A senha está ERRADA!";
+        certoOuErrado[3].src="https://win98icons.alexmeub.com/icons/png/msg_error-0.png";
+    }
+}
+function enigmaCinco(){
+    if (input[4].value === "956"){
+        resposta[4].innerHTML = "A senha está CORRETA!";
+        imagemPasta[4].src="https://i.imgur.com/6yfWk07.png";
+        certoOuErrado[4].src="https://win98icons.alexmeub.com/icons/png/trust0-0.png";
+    } else {
+        resposta[4].innerHTML = "A senha está ERRADA!";
+        certoOuErrado[4].src="https://win98icons.alexmeub.com/icons/png/msg_error-0.png";
+    }
+}
+function enigmaSeis(){
+    if (input[5].value === "803"){
+        resposta[5].innerHTML = "A senha está CORRETA!";
+        imagemPasta[5].src="https://i.imgur.com/6yfWk07.png";
+        certoOuErrado[5].src="https://win98icons.alexmeub.com/icons/png/trust0-0.png";
+    } else {
+        resposta[5].innerHTML = "A senha está ERRADA!";
+        certoOuErrado[5].src="https://win98icons.alexmeub.com/icons/png/msg_error-0.png";
+    }
+}
+function enigmaSete(){
+    if (input[6].value === "178"){
+        resposta[6].innerHTML = "A senha está CORRETA!";
+        imagemPasta[6].src="https://i.imgur.com/6yfWk07.png";
+        certoOuErrado[6].src="https://win98icons.alexmeub.com/icons/png/trust0-0.png";
+    } else {
+        resposta[6].innerHTML = "A senha está ERRADA!";
+        certoOuErrado[6].src="https://win98icons.alexmeub.com/icons/png/msg_error-0.png";
+    }
+}
+function enigmaOito(){
+    if (input[7].value === "658"){
+        resposta[7].innerHTML = "A senha está CORRETA!";
+        imagemPasta[7].src="https://i.imgur.com/6yfWk07.png";
+        certoOuErrado[7].src="https://win98icons.alexmeub.com/icons/png/trust0-0.png";
+    } else {
+        resposta[7].innerHTML = "A senha está ERRADA!";
+        certoOuErrado[7].src="https://win98icons.alexmeub.com/icons/png/msg_error-0.png";
+    }
+}
+function enigmaNove(){
+    if (input[8].value === "428"){
+        resposta[8].innerHTML = "A senha está CORRETA!";
+        imagemPasta[8].src="https://i.imgur.com/6yfWk07.png";
+        certoOuErrado[8].src="https://win98icons.alexmeub.com/icons/png/trust0-0.png";
+    } else {
+        resposta[8].innerHTML = "A senha está ERRADA!";
+        certoOuErrado[8].src="https://win98icons.alexmeub.com/icons/png/msg_error-0.png";
+    }
+}
