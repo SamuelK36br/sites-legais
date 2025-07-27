bd29369245dec5f0f2eda0fcf6eceb938296bf01
SamuelK36br <samuelbernardo091@gmail.com>
2025-07-27 17:35:44 -0300

    Adicionado operação potencia à Calculadora Liedshow

--- sites/calculadora-liedshow/script.js
+++ sites/calculadora-liedshow/script.js
@@ -1,15 +1,17 @@
 // -- SELETORES:
-const valor1 = document.getElementById('valor1');
-const valor2 = document.getElementById('valor2');
+const inputs = document.getElementsByTagName('input');
+const [valor1, valor2] = inputs;
+
+const valorTexto = document.getElementsByTagName('label');
+const [valTexto1, valTexto2] = valorTexto;
 
 const select = document.querySelector('select');
+const operacao = document.getElementsByTagName('option');
 
 const calcular = document.getElementById('calcular');
 
-const operacao = document.getElementsByTagName('option');
-
 const resultado = document.getElementById('resultado');
-
+// -----------------------------------------
 // -- SCRIPT:
 /* CONVERSAO STRING PARA NÚMERO: */
 function converter(numero) {
@@ -23,23 +25,25 @@ function somar(v1, v2) {
   let b = converter(v2);
   resultado.innerHTML = `Resultado: ${a + b}`;
 }
-
 function subtrair(v1, v2) {
   let a = converter(v1);
   let b = converter(v2);
   resultado.innerHTML = `Resultado: ${a - b}`;
 }
-
 function multiplicar(v1, v2) {
   let a = converter(v1);
   let b = converter(v2);
   resultado.innerHTML = `Resultado: ${a * b}`;
 }
-
 function dividir(v1, v2) {
   let a = converter(v1);
   let b = converter(v2);
   resultado.innerHTML = `Resultado: ${a / b}`;
+}
+function potencia(v1, v2) {
+  let a = converter(v1);
+  let b = converter(v2);
+  resultado.innerHTML = `Resultado: ${Math.pow(a, b)}`;
 }
 
 /* REALIZAR CÁLCULO: */
@@ -57,8 +61,12 @@ function realizarCalculo() {
   else if (operacao[4].selected) {
     dividir(valor1.value, valor2.value);
   }
+  else if (operacao[5].selected) {
+    potencia(valor1.value, valor2.value);
+  }
 }
 calcular.onclick = realizarCalculo;
+select.onchange = realizarCalculo;
 
 /* VERIFICAÇÕES: */
 setInterval(() => {
@@ -68,8 +76,9 @@ setInterval(() => {
     select.disabled = true;
     calcular.disabled=true;
   }
+  
   /* Verificação divisão por zero: */
-  else if (valor2.value.charAt(0).includes('0')) {
+  else if (valor2.value==="0") {
     operacao[4].disabled = true;
     select.disabled = false;
     calcular.disabled = false;
@@ -82,5 +91,16 @@ setInterval(() => {
   calcular.disabled = false;
   operacao[4].disabled=false;
   }
+  
+  /* Verificação potência selecionada: */
+  if (operacao[5].selected) {
+    valTexto1.innerHTML="Base:";
+    valor1.style.transform="translateX(13.7px)";
+    valTexto2.innerHTML="Potência:";
+  }
+  else {
+    valTexto1.innerHTML="Valor número 1:";
+    valor1.style.transform="translateX(0px)";
+    valTexto2.innerHTML="Valor número 2:";
+  }
 }, 0);
-select.onchange = realizarCalculo;
