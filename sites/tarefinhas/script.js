86591cb28da7dd57ba18263c31ab99489a8ea084
SamuelK36br <samuelbernardo091@gmail.com>
2026-04-24 22:02:13 -0300

    sites/tarefinhas: As tarefas agora são salvas e anexadas ao DOM, ao sair e entrar na página. (FINALMENTE!!!!!!!!!!!!!!!!)

--- sites/tarefinhas/script.js
+++ sites/tarefinhas/script.js
@@ -6,8 +6,8 @@ const stt_btn = document.querySelector("#stt");
 const form = document.querySelector("#criar-tarefa form");
 
 const audio = {
-	salvar: document.getElementById("#salvarTarefa-audio"),
-	apagar: document.getElementById("#apagarTarefa-audio")
+	salvar: document.getElementById("salvarTarefa-audio"),
+	apagar: document.getElementById("apagarTarefa-audio")
 }
 
 const input = document.querySelector("#criar-tarefa input");
@@ -19,7 +19,7 @@ const tarefasContainer = document.querySelector(".tarefas-container");
 form.onsubmit = e => e.preventDefault();
 
 // Criar Tarefas:
-function criarElementos() {
+function criarElementos(c = false, l = input.value) {
 	/* div.tarefa: */
 	const tarefa = document.createElement("div");
 	tarefa.classList.add("tarefa");
@@ -31,28 +31,31 @@ function criarElementos() {
 	/* checkbox: */
 	const checkbox = document.createElement("input");
 	checkbox.setAttribute("type", "checkbox");
+	checkbox.checked = c;
 	
 	/* label: */
 	const label = document.createElement("label");
-	label.innerText = input.value;
+	label.innerText = l;
 	
 	/* img (apagar tarefa): */
 	const apagarTarefa = document.createElement("img");
 	apagarTarefa.src = "assets/deletarTarefa.svg";
 	
 	/* Eventos: */
-	checkbox.onchange = function () {
+	checkbox.onchange = function() {
 		label.classList.toggle("cortado");
+		salvarElementosCriados();
 	}
 	
-	label.onclick = function () {
-		checkbox.checked = !checkbox.checked;
-		label.classList.toggle("cortado");
-	}
+	if (c) label.classList.add("cortado");
 	
-	apagarTarefa.onclick = function () {
+	apagarTarefa.onclick = function() {
 		const resposta = confirm("Tem certeza que quer apagar essa tarefa?");
-		if (resposta) tarefa.remove();
+		if (resposta) {
+			tarefa.remove();
+			audio.apagar.play();
+			salvarElementosCriados();
+		};
 	}
 	
 	/* Anexar elementos: */
@@ -61,9 +64,49 @@ function criarElementos() {
 	tarefa.appendChild(inputELabel);
 	tarefa.appendChild(apagarTarefa);
 	tarefasContainer.appendChild(tarefa);
+	
+	// Salvar elementos:
+	salvarElementosCriados();
+}
+salvarTarefa.onclick = () => {
+	if (!(input.validity.valueMissing)) {
+		audio.salvar.play();
+		criarElementos()
+	}
 }
 
-salvarTarefa.onclick = criarElementos;
+// Salvar elementos criados:
+function salvarElementosCriados() {
+	const checkboxes = document.querySelectorAll("input[type=checkbox]");
+	const labels = document.querySelectorAll(".input-e-label label");
+	
+	function Objeto(c, l) {
+		this.checkbox = c;
+		this.label = l;
+	}
+	
+	const arr = [];
+	for (let i = 0; i < labels.length; i++) {
+		const obj = new Objeto(checkboxes[i].checked, labels[i].innerText);
+		arr.push(obj)
+	}
+	
+	localStorage.setItem(
+		"tarefinhas-save",
+		JSON.stringify(arr, null, 2)
+	)
+	
+	console.log(localStorage.getItem("tarefinhas-save"));
+}
+
+// Criar elementos que estão no localStorage:
+function criarElementosPeloSave(item) {
+	JSON.parse(item).forEach(obj => {
+		criarElementos(obj.checkbox, obj.label);
+	})
+}
+const item = localStorage.getItem("tarefinhas-save");
+if (item) criarElementosPeloSave(item);
 
 // Voz para texto (stt):
 const captarVoz = new webkitSpeechRecognition();
@@ -73,9 +116,3 @@ captarVoz.onresult = e => input.value = e.results[0][0].transcript;
 captarVoz.onnomatch = () => alert("Não foi possível transcrever sua voz.");
 
 stt_btn.onclick = () => captarVoz.start();
-
-// Aviso ao sair:
-window.onbeforeunload = function(e) {
-	e.preventDefault();
-	e.returnValue = "";
-}
